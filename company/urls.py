e89829a3ed14c33d2d498dcb656ad263bac7ddc4
Charles Kidder <charles.kidder@meritknights.com>
2021-02-10 10:55:23 -0700

    sup2

--- company/urls.py
+++ company/urls.py
@@ -18,13 +18,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from project.persons.views import generate_image
+from .people.views import generate_image
 from django.views.generic import TemplateView
 from .products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('generate-image/', generate_image, name="generate-image")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    path('generate-image/', generate_image, name="generate-image"),
     path('products', views.view_products, name="products"),
     path('', TemplateView.as_view(template_name="index.html"))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
